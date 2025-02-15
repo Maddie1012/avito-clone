25c2d67faefc634a39ddc27c2434eed875c46f19
Мадина Карацева <madinakaratseva@MacBook-Pro-Madina.local>
2025-02-15 18:49:46 +0300

    Обновлен README файл

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import FormPage from "./pages/FormPage";
 import ListPage from "./pages/ListPage";
 import ItemPage from "./pages/ItemPage";
